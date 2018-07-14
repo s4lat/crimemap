88ff93cd3c1c5e7fcd4c3cf5ef36d18b03c1ba12
s4lat <zakazchikm@gmail.com>
2018-07-14 13:23:11 +0500

    db

--- db_setup.py
+++ db_setup.py
@@ -1,13 +1,12 @@
 import _mysql as mysqlclient
 import dbconfig
 
-connection = mysqlclient.connect('localhost',dbconfig.db_user,dbconfig.db_password)
+db = mysqlclient.connect('localhost',dbconfig.db_user,dbconfig.db_password)
 
 try:
-    with connection.cursor() as cursor:
-        sql = "CREATE DATABASE IF NOT EXISTS crimemap"
-        cursor.execute(sql)
-        sql = """CREATE TABLE IF NOT EXISTS crimemap.crimes (
+    sql = "CREATE DATABASE IF NOT EXISTS crimemap"
+    db.query(sql)
+    sql = """CREATE TABLE IF NOT EXISTS crimemap.crimes (
 id int NOT NULL AUTO_INCREMENT
 latitude FLOAT(10,6)
 longtitude FLOAT(10,6)
@@ -18,6 +17,6 @@ updated_at TIMESTAMP,
 PRIMARY KEY(id)
 )"""
         cursor.execute(sql)
-    connection.commit()
+    db.commit()
 finally:
-    connection.close()
+    db.close()
