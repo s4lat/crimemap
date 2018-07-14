4e832dcdf362b8bdf2e53366746ef302520dd252
s4lat <zakazchikm@gmail.com>
2018-07-14 15:43:33 +0500

    kek

--- crimemap.py
+++ crimemap.py
@@ -1,6 +1,36 @@
-from flask import Flask
+from dbhelper import DBHelper
+from flask import Flask, render_template
+from flask import request as flaskRequest
+import requests
 
 app = Flask(__name__)
+db = DBHelper()
+
+@app.route('/')
+def index():
+	try:
+		data = DB.get_all_inputs()
+	except Exception as e:
+		print(e)
+		data = None
+	return render_template('home.html', data=data)
+
+@app.route("/add", methods=["POST"])
+def add():
+	try:
+		data = flaskRequest.form.get('userinput')
+		DB.add_input(data)
+	except Exception as e:
+		print(e)
+	return index()
+
+@app.route("/clear")
+def clear():
+	try:
+		DB.clear_all()
+	except Exception as e:
+		print(e)
+	return home()
 
 if __name__ == "__main__":
 	app.run(port=5000, debug=True)
--- db_setup.py
+++ db_setup.py
@@ -1,12 +1,15 @@
-import _mysql as mysqlclient
+import MySQLdb
 import dbconfig
 
-db = mysqlclient.connect('localhost',dbconfig.db_user,dbconfig.db_password)
+connection = MySQLdb.connect(host='localhost',
+                     user=dbconfig.db_user,
+                     password=dbconfig.db_password)
 
 try:
-    sql = "CREATE DATABASE IF NOT EXISTS crimemap"
-    db.query(sql)
-    sql = """CREATE TABLE IF NOT EXISTS crimemap.crimes (
+    with connection.cursor() as cursor:
+        sql = "CREATE DATABASE IF NOT EXISTS crimemap"
+        cursor.execute(sql)
+        sql = """CREATE TABLE IF NOT EXISTS crimemap.crimes (
 id int NOT NULL AUTO_INCREMENT,
 latitude FLOAT(10,6),
 longtitude FLOAT(10,6),
@@ -16,7 +19,7 @@ description VARCHAR(1000),
 updated_at TIMESTAMP,
 PRIMARY KEY(id)
 )"""
-    db.query(sql)
-    db.commit()
+        cursor.execute(sql)
+    connection.commit()
 finally:
-    db.close()
+    connection.close()
